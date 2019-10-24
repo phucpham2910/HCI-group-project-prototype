5de7d734902f78971d98c131e0d2e67dcd5b0229
phucpham2910 <56959120+phucpham2910@users.noreply.github.com>
2019-10-24 12:15:10 +0200

    first one

--- Assets/VRGazeIndicator.cs
+++ Assets/VRGazeIndicator.cs
@@ -13,12 +13,6 @@ public class VRGazeIndicator : MonoBehaviour
     bool gvrStatus;
     public float gvrTimer;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
