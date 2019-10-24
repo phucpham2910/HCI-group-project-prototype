ce8fb473d8e0ca8d123aca8001413b2dc18518d6
phucpham2910 <56959120+phucpham2910@users.noreply.github.com>
2019-10-24 11:54:58 +0200

    Initial commit

---
+++ Assets/VRGazeIndicator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class VRGazeIndicator : MonoBehaviour
+{
+
+    public Image imgCircle;
+    public UnityEvent GVRClick;
+    public float totalTime = 2;
+    bool gvrStatus;
+    public float gvrTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gvrStatus)
+        {
+            gvrTimer += Time.deltaTime;
+            imgCircle.fillAmount = gvrTimer / totalTime;
+        }
+
+        if (gvrTimer > totalTime)
+        {
+            GVRClick.Invoke();
+        }
+    }
+
+    public void GVROn()
+    {
+        gvrStatus = true;
+    }
+
+    public void GVROff()
+    {
+        gvrStatus = false;
+        gvrTimer = 0;
+        imgCircle.fillAmount = 0;
+    }
+}
